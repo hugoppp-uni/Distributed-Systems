2fdcc382498a63c6dd51521645fd35d49182f2d9
Hugo Protsch <hugo@protsch.dev>
2022-11-11 18:45:31 +0100

    Add worker base

--- aufg2/vslab-caf-master/src/main.cpp
+++ aufg2/vslab-caf-master/src/main.cpp
@@ -74,7 +74,7 @@ struct client_state {
   group grp;
   int512_t task;
 
-  actor_ostream log(stateful_actor<client_state>* self){
+  actor_ostream log(stateful_actor<client_state>* self) const{
       return aout(self) << "[CLIENT " << task << "] ";
   }
 
@@ -88,14 +88,15 @@ behavior client(stateful_actor<client_state>* self, caf::group grp, int512_t tas
 
   self->state.log(self) << "sending task '" << task << "'" << std::endl;
   self->send(self->state.grp, task_atom_v, task);
+  self->send(self, idle_request_atom_v, int512_t{123});
 
   // TODO: Implement me.
   return {
-      [=](result_atom, int512_t task, int512_t result, long cpu_time, long rho_cyles) {
+      [=](result_atom, int512_t task, int512_t result, int cpu_time, int rho_cyles) -> void {
           self->state.log(self) << "got result for " << task << ":" << result << std::endl;
           self->quit();
       },
-      [=](idle_request_atom) ->int512_t {
+      [=](idle_request_atom) -> int512_t {
           self->state.log(self) << "got idle request, sending task" << std::endl;
           return self->state.task;
       },
@@ -105,13 +106,15 @@ behavior client(stateful_actor<client_state>* self, caf::group grp, int512_t tas
 void run_client(actor_system& sys, const config& cfg) {
   if (auto eg = sys.middleman().remote_group("vslab", cfg.host, cfg.port)){
 
+      std::vector<caf::actor> v;
       while (true) {
           cout << "Enter a number:" << std::endl;
           int512_t number;
           std::cin >> number;
 
           auto grp = *eg;
-          sys.spawn(client, grp, number);
+          auto a1 =sys.spawn(client, grp, number);
+          v.emplace_back(a1);
           std::this_thread::sleep_for(std::chrono::milliseconds(500));
       }
 
@@ -126,27 +129,51 @@ void run_client(actor_system& sys, const config& cfg) {
 struct worker_state {
   // The joined group.
   group grp;
+  int512_t task;
+
+  actor_ostream log(stateful_actor<worker_state>* self) const{
+    return aout(self) << "[WORKER " << task << "] ";
+  }
+
+  void send_idle_request(stateful_actor<worker_state>* self) const{
+      self->state.log(self) << "sending idle request" << std::endl;
+      self->send(self->state.grp, idle_request_atom_v);
+  }
+
 };
 
 behavior worker(stateful_actor<worker_state>* self, caf::group grp) {
   // Join group and save it to send messages later.
+  self->set_default_handler(drop);
   self->join(grp);
   self->state.grp = grp;
-  // TODO: Implement me.
-  // - Calculate rho.
-  // - Check for new messages in between.
-  return {};
+  self->state.send_idle_request(self);
+  return {
+      [=](task_atom, int512_t task) {
+          self->send(self->state.grp, result_atom_v, task, int512_t {9}, int {0}, int {0});
+          if (self->mailbox().empty()){
+              self->state.send_idle_request(self);
+          }
+          // TODO: Implement me.
+          // - Calculate rho.
+          // - Check for new messages in between.
+      },
+      [=](idle_response_atom, int512_t task){
+          self->state.log(self) << "got idle response: " << task;
+          self->state.task = task;
+      }
+  };
 }
 
 void run_worker(actor_system& sys, const config& cfg) {
   if (auto eg = sys.middleman().remote_group("vslab", cfg.host, cfg.port)) {
     auto grp = *eg;
-    // TODO: Spawn workers, e.g:
-    // sys.spawn(worker, grp);
+     sys.spawn(worker, grp);
+     sys.await_all_actors_done();
   } else {
     cerr << "error: " << caf::to_string(eg.error()) << '\n';
   }
-  sys.await_all_actors_done();
+  std::this_thread::sleep_for(std::chrono::hours(1));
 }
 
 // -- MAIN ---------------------------------------------------------------------
